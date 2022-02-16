074491cf54e98304d84070493ec44594b2ec3790
Swati Sehgal <swati.tcd@gmail.com>
2022-02-16 17:18:41 +0000

    podresource: watch: Kubelet changes to support listandwatch endpoint
    Signed-off-by: Swati Sehgal <swati.tcd@gmail.com>

--- pkg/kubelet/apis/podresources/server_v1.go
+++ pkg/kubelet/apis/podresources/server_v1.go
@@ -19,12 +19,14 @@ package podresources
 import (
 	"context"
 	"fmt"
+	"sync"
 
 	utilfeature "k8s.io/apiserver/pkg/util/feature"
 	kubefeatures "k8s.io/kubernetes/pkg/features"
 	"k8s.io/kubernetes/pkg/kubelet/metrics"
 
-	"k8s.io/kubelet/pkg/apis/podresources/v1"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/kubelet/pkg/apis/podresources/v1"
 )
 
 // podResourcesServerV1alpha1 implements PodResourcesListerServer
@@ -33,17 +35,48 @@ type v1PodResourcesServer struct {
 	devicesProvider DevicesProvider
 	cpusProvider    CPUsProvider
 	memoryProvider  MemoryProvider
+	podSource       chan podInfo
+	lock            sync.RWMutex
+	sinkId          int
+	podSinks        map[int]chan podInfo
+}
+
+type podInfo struct {
+	Action v1.WatchPodAction
+	Pod    *corev1.Pod
 }
 
 // NewV1PodResourcesServer returns a PodResourcesListerServer which lists pods provided by the PodsProvider
 // with device information provided by the DevicesProvider
 func NewV1PodResourcesServer(podsProvider PodsProvider, devicesProvider DevicesProvider, cpusProvider CPUsProvider, memoryProvider MemoryProvider) v1.PodResourcesListerServer {
-	return &v1PodResourcesServer{
+	p := &v1PodResourcesServer{
 		podsProvider:    podsProvider,
 		devicesProvider: devicesProvider,
 		cpusProvider:    cpusProvider,
 		memoryProvider:  memoryProvider,
+		podSource:       make(chan podInfo),
+		podSinks:        make(map[int]chan podInfo),
 	}
+	go p.dispatchPods()
+	return p
+}
+
+func (p *v1PodResourcesServer) makePodResources(pod *corev1.Pod) *v1.PodResources {
+	pRes := v1.PodResources{
+		Name:       pod.Name,
+		Namespace:  pod.Namespace,
+		Containers: make([]*v1.ContainerResources, len(pod.Spec.Containers)),
+	}
+
+	for j, container := range pod.Spec.Containers {
+		pRes.Containers[j] = &v1.ContainerResources{
+			Name:    container.Name,
+			Devices: p.devicesProvider.GetDevices(string(pod.UID), container.Name),
+			CpuIds:  p.cpusProvider.GetCPUs(string(pod.UID), container.Name),
+			Memory:  p.memoryProvider.GetMemory(string(pod.UID), container.Name),
+		}
+	}
+	return &pRes
 }
 
 // List returns information about the resources assigned to pods on the node
@@ -95,4 +128,78 @@ func (p *v1PodResourcesServer) GetAllocatableResources(ctx context.Context, req 
 		CpuIds:  p.cpusProvider.GetAllocatableCPUs(),
 		Memory:  p.memoryProvider.GetAllocatableMemory(),
 	}, nil
+}
+
+func (p *v1PodResourcesServer) ListAndWatch(req *v1.ListAndWatchPodResourcesRequest, srv v1.PodResourcesLister_ListAndWatchServer) error {
+	sinkId, sinkChan := p.registerWatcher()
+	defer p.unregisterWatcher(sinkId)
+	for {
+		pod := <-sinkChan
+		resp := p.makeWatchPodResponse(pod)
+		err := srv.Send(resp)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (p *v1PodResourcesServer) AddPod(pod *corev1.Pod) {
+	p.podSource <- podInfo{
+		Action: v1.WatchPodAction_ADDED,
+		Pod:    pod,
+	}
+}
+
+func (p *v1PodResourcesServer) UpdatePod(pod *corev1.Pod) {
+	p.podSource <- podInfo{
+		Action: v1.WatchPodAction_UPDATED,
+		Pod:    pod,
+	}
+}
+
+func (p *v1PodResourcesServer) DeletePod(pod *corev1.Pod) {
+	p.podSource <- podInfo{
+		Action: v1.WatchPodAction_DELETED,
+		Pod:    pod,
+	}
+}
+
+func (p *v1PodResourcesServer) dispatchPods() {
+	for {
+		info := <-p.podSource
+
+		p.lock.RLock()
+		for _, ch := range p.podSinks {
+			ch <- info
+		}
+		p.lock.RUnlock()
+	}
+}
+
+func (p *v1PodResourcesServer) registerWatcher() (int, chan podInfo) {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+	sinkChan := make(chan podInfo)
+	sinkId := p.sinkId
+	p.sinkId++
+	p.podSinks[sinkId] = sinkChan
+	return sinkId, sinkChan
+}
+
+func (p *v1PodResourcesServer) unregisterWatcher(sinkId int) {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+	// TODO: sink close?
+	delete(p.podSinks, sinkId)
+}
+
+func (p *v1PodResourcesServer) makeWatchPodResponse(info podInfo) *v1.ListAndWatchPodResourcesResponse {
+	resp := v1.ListAndWatchPodResourcesResponse{
+		Action: info.Action,
+		PodResources: []*v1.PodResources{
+			p.makePodResources(info.Pod),
+		},
+	}
+	return &resp
 }
--- pkg/kubelet/apis/podresources/types.go
+++ pkg/kubelet/apis/podresources/types.go
@@ -51,3 +51,9 @@ type MemoryProvider interface {
 	// GetAllocatableMemory returns the allocatable memory from the node
 	GetAllocatableMemory() []*podresourcesapi.ContainerMemory
 }
+
+type PodNotifier interface {
+	AddPod(pod *v1.Pod)
+	UpdatePod(pod *v1.Pod)
+	DeletePod(pod *v1.Pod)
+}
